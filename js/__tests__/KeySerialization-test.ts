29631275238d5a99064197b57455fcce71640824
Clement Brisset <clement.brisset@discordapp.com>
2025-05-05 15:48:39 -0700

    [js] re-organize JS package to be fetched by monorepo

--- js/__tests__/KeySerialization-test.ts
+++ js/__tests__/KeySerialization-test.ts
@@ -1,13 +1,12 @@
 import {describe, expect, test} from '@jest/globals';
-
-const DAVE = require('../libdave');
+import { serializeKey } from '../KeySerialization';
 
 describe('KeySerialization', () => {
   test('expectedOutput', async () => {
     const zeroData = new Uint8Array(6);
-    expect(DAVE.serializeKey(zeroData)).toBe('AAAAAAAA');
+    expect(serializeKey(zeroData)).toBe('AAAAAAAA');
 
     const moreData = new Uint8Array([0, 1, 0xff, 0x7f, 0x80]);
-    expect(DAVE.serializeKey(moreData)).toBe('AAH/f4A=');
+    expect(serializeKey(moreData)).toBe('AAH/f4A=');
   });
 });
